49deda25921daf5ed4b565f7ecf917444f8f6100
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-03-31 17:54:35 -0600

    v18 render update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
     <App />
