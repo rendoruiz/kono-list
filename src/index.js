a83be3c891a1a0f50fad36f0eb7e887b89160abd
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-03-31 17:56:00 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
-ReactDOM.render(
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
