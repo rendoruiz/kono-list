f87b6b52ef2401146d1169b17e3e6504a0012495
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-03-31 21:56:16 -0600

    reducer test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,71 @@
 import * as React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const defaultListArray = [
+  {
+    id: 0,
+    name: "Tasks",
+    date_created: Date.now(),
+    date_updated: null,
+    badge: "⭐",
+  },
+];
+
+const listArrayReducer = (state, action) => {
+  let newState;
+
+  switch(action.type) {
+    case 'LIST_CREATE':
+      const newList = {
+        ...action.payload,
+        id: uuidv4(),
+        date_created: Date.now(),
+        date_updated: null,
+      };
+      const newData = state.data;
+      newData.push(newList);
+      newState = { 
+        ...state,
+        data: newData,
+      };
+      break;
+    case 'LIST_UPDATE':
+      newState = state.data.map((list) => {
+        if (list.id === action.payload.id) {
+          const updatedItem = {
+            ...list,
+            name: action.payload.name,
+            badge: action.payload.badge ?? list.badge,
+            date_updated: Date.now(),
+          };
+          return updatedItem;
+        }
+        return list;
+      });
+      break;
+    case 'LIST_DELETE':
+      newState = {
+        ...state,
+        data: state.data.filter(
+          (list) => list.id !== action.id
+        ),
+      };
+      break;
+    default:
+      throw new Error();
+  }
+
+  localStorage.setItem(state.localKey, JSON.stringify(newState.data));
+  return newState;
+}
 
 const useLocalState = (key, initialState) => {
   const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
+    JSON.parse(localStorage.getItem(key)) || initialState
   );
 
   React.useEffect(() => {
-    localStorage.setItem(key, value);
+    localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
   return [value, setValue];
@@ -14,19 +73,39 @@ const useLocalState = (key, initialState) => {
 
 
 const App = () => {
-  const [isListViewOpen, setIsListViewOpen] = useLocalState('isListViewOpen', true);
+  const [isListViewOpen, setIsListViewOpen] = React.useState('isListViewOpen', true);
   const [isListItemEditorViewOpen, setIsListItemEditorViewOpen] = useLocalState('isListItemEditorViewOpen', false);
+  const [listArray, dispatchListArray] = React.useReducer(
+    listArrayReducer,
+    { 
+      data: JSON.parse(localStorage.getItem('lists')) ?? defaultListArray, 
+      localKey: 'lists',
+    }
+  );
+  console.log(listArray.data)
 
   const handleToggleListView = () => setIsListViewOpen(!isListViewOpen);
 
   const handleToggleListItemEditorView = () => setIsListItemEditorViewOpen(!isListItemEditorViewOpen);
 
+  const handleCreateList = () => {
+    dispatchListArray({
+      type: 'LIST_CREATE',
+      payload: {
+        name: "test",
+        badge: "😚",
+      }
+    })
+  }
+
   return (
-    <div>
+    <div className='h-screen w-screen bg-blue-300'>
       {/* list view */}
       <ListView 
         isOpen={isListViewOpen} 
         onToggleView={handleToggleListView} 
+        data={listArray.data}
+        onCreateList={handleCreateList}
       />
 
       {/* list item view */}
@@ -40,25 +119,39 @@ const App = () => {
   );
 }
 
-const ListView = ({ isOpen, onToggleView }) => {
-  
-  return (
-    <div>
-      <header></header>
 
-      <main>
+const ListView = ({ isOpen, onToggleView, data, onCreateList }) => {
+  return (
+    <div className='relative grid grid-rows-[auto,1fr,auto] h-full w-80'>
+      <header className='p-5 bg-blue-600'></header>
+
+      <main className=''>
+        <ul>
+          {JSON.stringify(data)}
+          {data.map((list) => (
+            <List
+              key={list.id}
+              list={list}
+            />
+          ))}
+        </ul>
 
       </main>
 
-      <footer></footer>
+      <footer className='sticky bottom-0 p-5 bg-blue-600'>
+        <button onClick={onCreateList}>New List</button>
+      </footer>
     </div>
   )
 }
 
-const List = () => (
-  <div>
-
-  </div>
+const List = ({ list }) => (
+  <li>
+    <button>
+      <span className='w-5 h-5 font-mono leading-none'>{list.badge}</span>
+      <p>{list.name}</p>
+    </button>
+  </li>
 )
 
 
