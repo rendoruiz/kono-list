7d309062fa21b11d0b80311ed87ee254053fc68b
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-03-31 18:36:31 -0600

    main components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
-function App() {
+const useLocalState = (key, initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
+
+const App = () => {
+  const [isListViewOpen, setIsListViewOpen] = useLocalState('isListViewOpen', true);
+  const [isListItemEditorViewOpen, setIsListItemEditorViewOpen] = useLocalState('isListItemEditorViewOpen', false);
+
+  const handleToggleListView = () => setIsListViewOpen(!isListViewOpen);
+
+  const handleToggleListItemEditorView = () => setIsListItemEditorViewOpen(!isListItemEditorViewOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* list view */}
+      <ListView 
+        isOpen={isListViewOpen} 
+        onToggleView={handleToggleListView} 
+      />
+
+      {/* list item view */}
+
+      {/* list item editor view */}
+      <ListItemEditorView
+        isOpen={isListItemEditorViewOpen}
+        onToggleView={handleToggleListItemEditorView}
+      />
     </div>
   );
+}
+
+const ListView = ({ isOpen, onToggleView }) => {
+  
+  return (
+    <div>
+      <header></header>
+
+      <main>
+
+      </main>
+
+      <footer></footer>
+    </div>
+  )
+}
+
+const List = () => (
+  <div>
+
+  </div>
+)
+
+
+
+const ListItemView = () => (
+  <div>
+
+  </div>
+)
+
+const ListItem = () => {
+  <div>
+
+  </div>
+}
+
+
+
+const ListItemEditorView = ({ isOpen, onToggleView }) => {
+  
+  return (
+    <div>
+
+    </div>
+  )
 }
 
 export default App;
