5ed00b4601576148f4b8f6cf4ec31b81cd8c3256
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-02 15:41:37 -0600

    better fix

--- src/App.js
+++ src/App.js
@@ -463,8 +463,8 @@ const ListEditorView = ({ isOpen, listData, onUpdateList, onCancelCreate }) => {
 
 
 const ListItemView = ({ listItemRowsData, selectedListItemData, selectedListData, onToggleListEditView, onSelectListItem, onCreateListItem, onUpdateListItemCheckState, onDeleteList, onUpdateListCheckedItemState }) => (
-  <div className='grid pt-2'>
-    <div className='relative grid grid-rows-[auto,1fr,auto] rounded-tl-xl px-10 w-full h-full max-h-[calc(100vh-8px)] overflow-scroll bg-blue-200'>
+  <div className='grid pt-2 max-h-screen'>
+    <div className='relative grid grid-rows-[auto,1fr,auto] rounded-tl-xl px-10 w-full h-full overflow-scroll bg-blue-200'>
       <header className='sticky top-0 flex items-center justify-between pt-10 pb-5 bg-blue-200/90'>
         {/* edit list */}
         <button 
