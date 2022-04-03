29a632d96d330c9b4159c2b9757bce9a1fba78c4
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-03 12:33:52 -0600

    toggle button

--- src/components/Task/TaskEditorPanel.js
+++ src/components/Task/TaskEditorPanel.js
@@ -1,12 +1,38 @@
+import TaskCompleteToggleButton from "./TaskCompleteToggleButton";
+
 const TaskEditorPanel = ({ 
   task, 
-  onClosePanel
+  selectedList,
+  onClosePanel,
+  onToggleTaskCompleteState
 }) => {
   return task && (
-    <div className='w-80 max-h-screen bg-green-300/30'>
-      <p className='font-mono font-medium text-xs uppercase break-word'>
-        {JSON.stringify(task)?.replaceAll(',"', ', "')}
-      </p>
+    <div className='w-full max-w-xs max-h-screen bg-green-300/30'>
+      {/* mobile navigation & list name / close panel */}
+      <header>
+        <p className='font-mono font-medium text-xs uppercase break-word'>
+          {JSON.stringify(task)?.replaceAll(',"', ', "')}
+        </p>
+      </header>
+
+      <main className="grid">
+        <form>
+          {/* task toggle & name */}
+          <div>
+            
+
+          <TaskCompleteToggleButton
+            task={task}
+            onToggle={() => onToggleTaskCompleteState(task)}
+          />
+          </div>
+        </form>
+      </main>
+
+      {/* date created, delete */}
+      <footer>
+
+      </footer>
     </div>
   )
 }
