85775532663ead301b88d67d50ddc7aeb9d93169
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-03 17:04:52 -0600

    note indicator

--- src/components/Task/TaskPanelListRow.js
+++ src/components/Task/TaskPanelListRow.js
@@ -7,7 +7,7 @@ const TaskPanelListRow = ({
   onToggleTaskCompleteState 
 }) => (
   <li className={
-    'grid grid-cols-[auto,1fr] rounded-md cursor-pointer hover:bg-white/90 ' + 
+    'grid grid-cols-[auto,1fr] rounded-md py-1 min-h-[50px] cursor-pointer hover:bg-white/90 ' + 
     (task.id === selectedTask?.id ? 'bg-white' : 'bg-white/70')
   }>
     {/* <p className='mt-2 mb-3 font-mono font-medium text-xs uppercase break-word'>{JSON.stringify(task).replaceAll(',"', ', "')}</p> */}
@@ -23,11 +23,20 @@ const TaskPanelListRow = ({
       type='button'
       title='Select task'
       onClick={() => onSelectTask(task)}
-      className='flex-1 py-4 pr-2 text-sm text-left'
+      className='flex-1 grid content-center pr-2 text-sm text-left'
     >
+      {/* title */}
       <p className={task.is_complete ? 'opacity-60 line-through' : ''}>
         {task?.title}
       </p>
+
+      {/* note presence indicator */}
+      {task.note && task.note.length > 0 && (
+        <p className=' mt-[2px] text-xs'>
+          <span>📝</span>
+          <span className="ml-[2px] text-black/60">Note</span>
+        </p>
+      )}
     </button>
   </li>
 );
