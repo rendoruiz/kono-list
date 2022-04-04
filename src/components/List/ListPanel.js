3cba4845832b7050e0daf626f3f144b07dbfd42b
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-03 21:02:39 -0600

    list panel complete

--- src/components/List/ListPanel.js
+++ src/components/List/ListPanel.js
@@ -20,10 +20,10 @@ const ListPanel = ({
 
     {/* panel */}
     <div className={
-      'absolute inset-0 right-auto z-30 grid grid-rows-[auto,1fr,auto] w-full h-full max-h-screen bg-slate-100 overflow-scroll transition-transform duration-200 bp520:w-72 md:relative md:translate-x-0 md:transition-none  ' +
+      'absolute inset-0 right-auto z-30 grid grid-rows-[1fr,auto] w-full h-screen bg-slate-100 transition-transform duration-200 bp520:w-72 md:relative md:translate-x-0 md:transition-none  ' +
       (isOpen ? 'md:translate-x-0' : '-translate-x-full')
     }>
-      <main>
+      <main className='overflow-y-auto'>
         {/* list items */}
         <ul className='grid py-1'>
           {listItems.map((list) => (
@@ -37,13 +37,7 @@ const ListPanel = ({
         </ul>
       </main>
 
-      <header className='-order-1 sticky top-0 border-b-2 py-3 px-5 '>
-        <button
-          onClick={onTogglePanel}
-        >
-          toggle
-        </button>
-      </header>
+      {/* <header className='-order-1 sticky top-0 border-b-2 py-3 px-5 '></header> */}
 
       <footer className='sticky bottom-0 border-t-2 py-[2px]'>
         {/* add new list button */}
