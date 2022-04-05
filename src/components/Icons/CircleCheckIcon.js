134e929eeb91928ec521cd32a17d519bf17fed40
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-04 20:51:01 -0600

    change complete toggle from html elements to svg

--- src/components/Icons/CircleCheckIcon.js
+++ src/components/Icons/CircleCheckIcon.js
@@ -1,5 +1,5 @@
 // https://tablericons.com/
-const CircleCheckIcon = ({ className }) => (
+const CircleCheckIcon = ({ className, circleClassName, checkClassName }) => (
   <svg
     className={className ?? 'stroke-current'}
     fill="none"
@@ -9,8 +9,8 @@ const CircleCheckIcon = ({ className }) => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path d="M0 0h24v24H0z" stroke="none" />
-    <path d="M9 12l2 2 4-4" />
-    <circle cx="12" cy="12" r="9" />
+    <circle cx="12" cy="12" r="9" className={circleClassName ?? ""} />
+    <path d="M9 12l2 2 4-4" className={checkClassName ?? ""} />
   </svg>
 );
  
