47d157bf19cb62807a1fe9d17c15efa7940e4c3d
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-23 13:19:04 -0600

    rename to taskpanelcreator

--- src/components/Task/TaskPanel/TaskPanel.js
+++ src/components/Task/TaskPanel/TaskPanel.js
@@ -5,7 +5,7 @@
  */
 
 import { listTemplate } from "../../../data/list";
-import TaskCreatorPanel from "../TaskCreatorPanel";
+import TaskCreatorPanel from "../TaskPanelCreator";
 import TaskPanelList from "./TaskPanelList";
 import ArrowLeftIcon from "../../Icons/ArrowLeftIcon";
 import PencilIcon from "../../Icons/PencilIcon";
