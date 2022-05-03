dc88cf57765bd8d16f6992095dfb9faf4b1087c9
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-05-02 23:22:21 -0600

    style update

--- src/components/List/ListPanel/ListPanelRow.js
+++ src/components/List/ListPanel/ListPanelRow.js
@@ -29,8 +29,12 @@ const ListPanelRow = ({
     ...listeners,
   }
   const sortableButtonClass = (
-    'transition origin-center ease-in-out delay-100 duration-300 ' +
-    ((dragItemId && (dragItemId !== list.id)) ? 'scale-x-[0.925] scale-y-90 opacity-60' : '')
+    'ease-in-out duration-300 transform-none bp520:transition bp520:origin-center bp520:delay-100 ' +
+    ((dragItemId && (dragItemId !== list.id)) ? 'bp520:scale-x-[0.925] bp520:scale-y-90 bp520:opacity-60' : '')
+  );
+  const sortableButtonBackplateClass = (
+    ' transition ' +
+    ((dragItemId === list.id) ? 'shadow-md shadow-black/25 bp520:shadow-none' : '')
   );
 
   const appDefaultListDividerClass = list.id === APP_LIST_ID.TASKS ? 'pb-[2px] mb-[2px] border-b-[2px]' : '';
@@ -49,9 +53,10 @@ const ListPanelRow = ({
       >
         {/* backplate */}
         <div className={
-          'relative grid grid-cols-[auto,1fr] items-center rounded py-1 group-hover:bg-slate-500/10 group-active:bg-slate-500/20 bp520:py-0 ' + 
-          (selectedList?.id === list.id ? 'bg-slate-500/10 before:absolute before:inset-y-3 before:left-0 before:rounded-full before:w-1 before:bg-blue-600' : '')}
-        >
+          'relative grid grid-cols-[auto,1fr] items-center rounded-sm py-1 group-hover:bg-slate-500/10 bp520:rounded bp520:py-0 bp520:group-active:bg-slate-500/20 ' + 
+          (selectedList?.id === list.id ? 'bg-slate-500/10 before:absolute before:inset-y-3 before:left-0 before:rounded-full before:w-1 before:bg-blue-600' : '') +
+          sortableButtonBackplateClass
+        }>
           {/* list icon */}
           <div className='grid place-items-center w-10 h-10'>
             <span className='font-mono text-lg leading-none'>
