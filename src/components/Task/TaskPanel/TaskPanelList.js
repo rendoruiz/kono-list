c0812ab23edb138b13a06a4cac2c86a29d82a41a
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-05-03 15:48:26 -0600

    split into different components

--- src/components/Task/TaskPanel/TaskPanelList.js
+++ src/components/Task/TaskPanel/TaskPanelList.js
@@ -24,67 +24,98 @@ const TaskPanelList = ({
 
   return (
     <main className='pt-1 pb-16 px-2 overflow-y-auto md:pb-1 md:px-0'>
-      {/* debug list */}
-      {/* <p className='mt-2 mb-3 font-mono font-medium text-xs uppercase break-word'>{JSON.stringify(selectedList).replaceAll(',"', ', "')}</p> */}
-
-      {/* tasks - incomplete */}
       {incompleteTasks && incompleteTasks.length > 0 && (
-        <ul className='grid content-start gap-[3px]'>
-          {incompleteTasks.map((task) => (
-            <TaskPanelListRow
-              key={task.id}
-              task={task}
+        <IncompleteTasks
+          taskItems={incompleteTasks}
+          selectedTask={selectedTask}
+          onSelectTask={onSelectTask}
+          onToggleTaskCompleteState={onToggleTaskCompleteState}
+        />
+      )}
+
+      {completedTasks && completedTaskCount > 0 && (
+        <>
+          <CompletedTaskVisibilityToggle
+            isHidden={!selectedList?.is_completed_hidden}
+            completedTaskCount={completedTaskCount}
+            onToggleCompletedItemsVisibility={onToggleCompletedItemsVisibility}
+          />
+
+          {!selectedList?.is_completed_hidden && (
+            <CompletedTasks
+              taskItems={completedTasks}
               selectedTask={selectedTask}
               onSelectTask={onSelectTask}
               onToggleTaskCompleteState={onToggleTaskCompleteState}
             />
-          ))}
-        </ul>
-      )}
-
-      {/* completed tasks section */}
-      {completedTasks && completedTaskCount > 0 && (
-        <>
-          {/* is_complete task toggle */}
-          <button 
-            type='button'
-            className='flex items-center rounded my-2 px-2 py-3 w-full leading-none first:!mt-0 hover:bg-black/10 sm:justify-self-start sm:py-1 sm:w-auto sm:bg-white/80 sm:text-sm sm:text-black/90 sm:hover:bg-white/90 sm:active:bg-white'
-            onClick={onToggleCompletedItemsVisibility}
-          >
-            {/* caret */}
-            <ChevronRightIcon className={
-              'w-5 h-5 stroke-[2.5] stroke-current transition-transform origin-center bp520:stroke-[1.5] ' +
-              (!selectedList?.is_completed_hidden ? 'rotate-90' : '')
-            } />
-
-            <span className='ml-1 mr-2'>
-              Completed
-            </span>
-            
-            {/* completed task count */}
-            <span className='pr-1 font-medium'>
-              {completedTaskCount}
-            </span>
-          </button>
-
-          {/* tasks - completed */}
-          {!selectedList?.is_completed_hidden && (
-            <ul className='grid content-start gap-[3px]'>
-              {completedTasks.map((task) => (
-                <TaskPanelListRow
-                  key={task.id}
-                  task={task}
-                  selectedTask={selectedTask}
-                  onSelectTask={onSelectTask}
-                  onToggleTaskCompleteState={onToggleTaskCompleteState}
-                />
-              ))}
-            </ul>
           )}
         </>
       )}
     </main>
   )
 }
+
+const IncompleteTasks = ({
+  taskItems,
+  selectedTask,
+  onSelectTask,
+  onToggleTaskCompleteState,
+}) => (
+  <ul className='grid content-start gap-[3px]'>
+    {taskItems.map((task) => (
+      <TaskPanelListRow
+        key={task.id}
+        task={task}
+        selectedTask={selectedTask}
+        onSelectTask={onSelectTask}
+        onToggleTaskCompleteState={onToggleTaskCompleteState}
+      />
+    ))}
+  </ul>
+);
+
+const CompletedTasks = ({
+  taskItems,
+  selectedTask,
+  onSelectTask,
+  onToggleTaskCompleteState,
+}) => (
+  <ul className='grid content-start gap-[3px]'>
+    {taskItems.map((task) => (
+      <TaskPanelListRow
+        key={task.id}
+        task={task}
+        selectedTask={selectedTask}
+        onSelectTask={onSelectTask}
+        onToggleTaskCompleteState={onToggleTaskCompleteState}
+      />
+    ))}
+  </ul>
+);
+
+const CompletedTaskVisibilityToggle = ({
+  isHidden,
+  completedTaskCount,
+  onToggleCompletedItemsVisibility,
+}) => (
+  <button 
+    type='button'
+    className='flex items-center rounded my-2 px-2 py-3 w-full leading-none first:!mt-0 hover:bg-black/10 sm:justify-self-start sm:py-1 sm:w-auto sm:bg-white/80 sm:text-sm sm:text-black/90 sm:hover:bg-white/90 sm:active:bg-white'
+    onClick={onToggleCompletedItemsVisibility}
+  >
+    <ChevronRightIcon className={
+      'w-5 h-5 stroke-[2.5] stroke-current transition-transform origin-center bp520:stroke-[1.5] ' +
+      (isHidden ? 'rotate-90' : '')
+    } />
+
+    <span className='ml-1 mr-2'>
+      Completed
+    </span>
+    
+    <span className='pr-1 font-medium'>
+      {completedTaskCount}
+    </span>
+  </button>
+);
  
 export default TaskPanelList;
