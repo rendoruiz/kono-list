d17a89dc504dd87e5822510ff0bd293838d8834d
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-05-03 20:08:22 -0600

    split to diff components

--- src/components/List/ListPanel/ListPanel.js
+++ src/components/List/ListPanel/ListPanel.js
@@ -24,16 +24,11 @@ const ListPanel = ({
   onReorderListItems,
 }) => (
   <>
-    {/* mobile backdrop toggle */}
-    <div 
-      className={
-        'fixed inset-0 z-30 transition-opacity duration-300 bg-black/70 md:hidden ' +
-        (isOpen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full')
-      }
-      onClick={onTogglePanel}
+    <MobileToggleableBackdrop
+      isOpen={isOpen}
+      onToggle={onTogglePanel}
     />
 
-    {/* panel container */}
     <div className={
       'fixed inset-0 right-auto z-30 grid grid-rows-[auto,1fr,60px] w-full h-screen bg-slate-100 transition-transform duration-200 bp520:grid-rows-[auto,1fr,auto] bp520:w-72 md:relative md:z-auto md:translate-x-0 md:transition-none ' +
       (isOpen ? 'md:translate-x-0' : '-translate-x-full')
@@ -44,11 +39,10 @@ const ListPanel = ({
         </h1>
       </header>
       
-      {/* list rows */}
       <main className='overflow-y-auto pt-3 py-1 bp520:pt-2'>
         {listItems && (
           <>
-            <SortableList
+            <ListRows
               listItems={listItems}
               selectedList={selectedList}
               onSelectList={onSelectList}
@@ -59,39 +53,27 @@ const ListPanel = ({
       </main>
 
       <footer className='fixed inset-0 top-auto grid grid-cols-[1fr,auto] border-t-2 p-[2px] bg-inherit text-black/70 bp520:sticky'>
-        {/* add new list button */}
-        <button 
-          type='button'
-          title='New list: add a list'
-          onClick={onCreateList}
-          className='group grid p-[2px] leading-none'
-        >
-          <div className='grid grid-cols-[auto,1fr] items-center rounded w-full group-hover:bg-slate-500/10 group-active:bg-slate-500/30'>
-            <div className='grid place-items-center p-2 w-12 h-12 bp520:w-10 bp520:h-10'>
-              <PlusIcon className='w-7 h-7 stroke-current bp520:w-6 bp520:h-6' />
-            </div>
-            <span className='text-left'>
-              New list
-            </span>
-          </div>
-        </button>
-
-        <button
-          type='button'
-          title='Open settings'
-          onClick={onToggleSettingsPanel}
-          className='group grid p-[2px] leading-none'
-        >
-          <div className='grid place-items-center rounded w-12 h-12 group-hover:bg-slate-500/10 group-active:bg-slate-500/30 bp520:w-10 bp520:h-10'>
-            <SettingsIcon className='w-7 h-7 stroke-current bp520:w-6 bp520:h-6' />
-          </div>
-        </button>
+        <NewListButton onCreateList={onCreateList} />
+        <SettingsButton onToggle={onToggleSettingsPanel} />
       </footer>
     </div>
   </>
 );
 
-const SortableList = ({
+const MobileToggleableBackdrop = ({
+  isOpen,
+  onToggle,
+}) => (
+  <div 
+    className={
+      'fixed inset-0 z-30 transition-opacity duration-300 bg-black/70 md:hidden ' +
+      (isOpen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full')
+    }
+    onClick={onToggle}
+  />
+);
+
+const ListRows = ({
   listItems,
   selectedList,
   onSelectList,
@@ -149,5 +131,36 @@ const SortableList = ({
     </DndContext>
   );
 }
+
+const NewListButton = ({ onCreateList }) => (
+  <button 
+    type='button'
+    title='New list: add a list'
+    onClick={onCreateList}
+    className='group grid p-[2px] leading-none'
+  >
+    <div className='grid grid-cols-[auto,1fr] items-center rounded w-full group-hover:bg-slate-500/10 group-active:bg-slate-500/30'>
+      <div className='grid place-items-center p-2 w-12 h-12 bp520:w-10 bp520:h-10'>
+        <PlusIcon className='w-7 h-7 stroke-current bp520:w-6 bp520:h-6' />
+      </div>
+      <span className='text-left'>
+        New list
+      </span>
+    </div>
+  </button>
+);
+
+const SettingsButton = ({ onToggle }) => (
+  <button
+    type='button'
+    title='Open settings'
+    onClick={onToggle}
+    className='group grid p-[2px] leading-none'
+  >
+    <div className='grid place-items-center rounded w-12 h-12 group-hover:bg-slate-500/10 group-active:bg-slate-500/30 bp520:w-10 bp520:h-10'>
+      <SettingsIcon className='w-7 h-7 stroke-current bp520:w-6 bp520:h-6' />
+    </div>
+  </button>
+);
  
 export default ListPanel;
